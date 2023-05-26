a4fd2a0e26d019aca89b7c82799c254da5ed5ccf
timkerekes <timothy.kerekes@gmail.com>
2023-05-26 12:08:08 -0500

    fixed syntax errors in setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,17 @@ setup(
     name='pgbackup',
     version='0.1.0',
     author='Tim Kerekes',
-    author_email='timothy.kerekes@gmail.com'
+    author_email='timothy.kerekes@gmail.com',
     description='A utility for backing up PostgreSQL databases.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/linuxacademy/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ]
+    }
 )
